591759565453ebe0e2e2efc8f5414e11ac086d0d
Mahalix <dominikmachala03@gmail.com>
2024-03-16 11:31:55 +0100

    Tworzenia klasy StandardGradeBook i dodanie konstruktora

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook.GradeBooks
+{
+    internal class StandardGradeBook:BaseGradeBook
+    {
+        public string Name { get; set; }
+        public StandardGradeBook(string name) : base(name)
+        {
+            Type = Enums.GradeBookType.Standard;
+        }
+    }
+}
