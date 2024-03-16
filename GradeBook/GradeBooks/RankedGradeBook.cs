a3faa5160bfbab701b2ba001fab15c2e9d0a97cb
Mahalix <dominikmachala03@gmail.com>
2024-03-16 14:54:52 +0100

    tworzenie metody GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -36,5 +36,37 @@ namespace GradeBook.GradeBooks
                 base.CalculateStudentStatistics(name);
             }
         }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            
+            var grade = Students.Select(y => y.AverageGrade).OrderByDescending(y => y).ToList();
+            var thresholds = grade.Count / 5;
+            var count = 0;
+            var finalgrade = 5;
+
+            foreach (var g in grade)
+            {
+                if (averageGrade >= g) break;
+
+                count++;
+                if (count >= thresholds)
+                {
+                    count -= thresholds;
+                    finalgrade--;
+                }
+            }
+            return finalgrade switch
+            {
+                5 => 'A',
+                4 => 'B',
+                3 => 'C',
+                2 => 'D',
+                _ => 'F'
+            };
+        }
     }
 }
